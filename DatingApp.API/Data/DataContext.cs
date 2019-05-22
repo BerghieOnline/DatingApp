63bc110c30e56ddf73adad37df2c0d6792b22d6c
Shawn Bergh <shawn@cfw.co.za>
2019-05-22 15:47:25 +0200

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext (DbContextOptions<DataContext> options) : base (options) { }
+
+        public DbSet<Value> Values { get; set; }    //Pass Value (Entity) to the DB. Values is the table Name in SQL
+
+
+    }
+}
