47c3814bd05934428123f5cb7f65e0164f69bac7
Shawn Bergh <shawn@cfw.co.za>
2019-05-24 12:10:56 +0200

    End of Section 5

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ model: any = {};
     this.authService.login(this.model).subscribe(next => {
       console.log('Logged in Succussfully');
     }, error => {
-        console.log('Failed to Login');
+        console.log(error);
     });
   }
 
